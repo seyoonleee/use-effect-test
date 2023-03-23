d62bf235f6e58ec67d90900aa0b063bdb42bd437
Seyoon <seyoonleee@gmail.com>
2023-03-23 18:07:59 +0900

    asdf

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  console.log('컴포넌트 랜더!');
+  const [count, setCount] = useState(0);
+  console.log(count);
+
+  // useEffect함수의 두번째 인자는 옵셔널(있어도 되고 없어도 되고)
+  useEffect(() => {
+    console.log('✨useEffect의 콜백함수 본문시작');
+    console.log('✨useEffect의 콜백실행!(오래걸리는 통신!)');
+    console.log('✨useEffect의 콜백함수 본문끝');
+  });
+
+  return <header>{console.log('DOM 마운트!')}App component</header>;
 }
 
 export default App;
